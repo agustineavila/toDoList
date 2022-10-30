739446deca293f35335ad8b745aa6ddafaa19f49
Agustin Avila <agustin.e.avila@gmail.com>
2022-10-30 17:30:30 -0300

    agregue localStorage para no perder datos

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const todos = [];
+const todos = JSON.parse(localStorage.getItem('todos')) || [];
 
 const render = () => {
 		const todoList = document.getElementById('todo-list');
@@ -9,13 +9,19 @@ const render = () => {
 				elemento.addEventListener('click', () => {
 					elemento.parentNode.removeChild(elemento);	
 					todos.splice(i, 1);
+					actualizaTodos(todos);
 					render();
-					console.log(todos, i);
 				})
 		})
 };
 
+const actualizaTodos = (todos) => {
+		const todoStrings = JSON.stringify(todos)
+		localStorage.setItem('todos', todoStrings)
+}
+
 window.onload = () => {
+	render()
 	const form = document.getElementById('todo-form');  // capturamos el form
 	form.onsubmit = (e) => {   
 		e.preventDefault();                              // evitamos que recargue
@@ -23,6 +29,7 @@ window.onload = () => {
 		const todoText = todo.value;       //guardamos el text que ingresamos
 		todo.value = '';     // borramos el contenido del input
 		todos.push(todoText);  // agregamos al array el texto
+		actualizaTodos(todos);
 		render();
 	}
 }
